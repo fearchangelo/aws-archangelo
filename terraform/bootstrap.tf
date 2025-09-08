d5e8412331c4b6586ab85f121857932bef06afbf
Felipe Archangelo <146233475+fearchangelo@users.noreply.github.com>
2025-09-08 16:47:04 -0700

    Delete terraform directory

--- terraform/bootstrap.tf
+++
@@ -1,5 +0,0 @@
-module "bootstrap" {
-	source =  "github.com/build-on-aws/terraform-samples//modules/bootstrap-aws-account"
-	state_file_aws_region =  "ca-central-1"
-	state_file_bucket_name = "aws-archangelo-terraform"
-}
