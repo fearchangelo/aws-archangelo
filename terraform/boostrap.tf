b72cc31b58741abf0f107ba12f8296cd33033ad9
fearchangelo <felipe.archangelo@gmail.com>
2025-09-09 01:24:16 +0000

    Adding CI/CD pipeline

--- terraform/boostrap.tf
+++ terraform/boostrap.tf
@@ -1,6 +1,18 @@
 module "bootstrap" {
     source = "github.com/build-on-aws/terraform-samples//modules/bootstrap-aws-account"
 
-    state_file_aws_region  = "us-west-2"
+    state_file_aws_region  = "us-east-2"
     state_file_bucket_name = "aws-archangelo-terraform"
 }
+
+module "bootstrap_cicd_aws_codebuild" {
+    source = "github.com/build-on-aws/terraform-samples//modules/bootstrap-cicd-aws-codebuild"
+
+    github_organization       = "fearchangelo"
+    github_repository         = "aws-archangelo"
+    aws_region                = "us-east-2"
+    state_file_iam_policy_arn = module.bootstrap.state_file_iam_policy_arn
+
+    codebuild_terraform_version = "1.9.7"
+}
+
